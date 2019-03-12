029906e7cc84dfc025315e976d7359989c926e35
gunwoo7 <b9635005@naver.com>
2019-03-13 00:09:55 +0900

    Relocation pictures, code's legibility

--- GUI_MANAGEMENT.py
+++ GUI_MANAGEMENT.py
@@ -5,22 +5,19 @@ import tkinter as tk
 
 win = tk.Tk()
 
+#*** first settings ***#
 #*** wallpaper - Settings ***#
 win.title("Typing-Program_new")
 win.geometry("768x432")
-wall = tk.PhotoImage(file = "wallpaper-768x432.gif")
+wall = tk.PhotoImage(file = "pictures\wallpaper-768x432.gif")
 wall_label = tk.Label(image = wall)
 wall_label.place(x = -2, y = -2)
-#*** Settings ***#
+#*** END ***#
 
-#*** Test code ***#
-def click_me():
-    action.configure(text="** I have been clicked")
+pixel = tk.PhotoImage(file='pictures\pixel.gif') # Use to change to Button size #
+#*** END ***#
 
-pixel = tk.PhotoImage(file='pixel.gif')
-action = tk.Button(win,text = "Click me!",command=click_me,image=pixel,compound='center')
-action.grid(column=1, row=0)
-action.config(width=100,height=100)
-#*** Test code ***#
+
+
 
 win.mainloop()
