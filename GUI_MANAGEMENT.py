520cdd4af8e930a5701c0855bc6cc15d365795bc
gunwoo7 <b9635005@naver.com>
2019-09-13 16:44:52 +0900

    Add words file, connect words file to GUI_management.py

--- GUI_MANAGEMENT.py
+++ GUI_MANAGEMENT.py
@@ -3,6 +3,7 @@
 
 
 import sys
+import random
 from PyQt5 import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -10,16 +11,21 @@ from PyQt5.QAxContainer import *
 from PyQt5.QtGui import *
 import Get_word_dictionary as gwd
 
-# First_page
+
 app = QApplication(sys.argv)
 pass_the_stage = 0
+selected_level = 0
+selected_word = "치킨"
+now_check_word = "\0"
 stage = 1
 step = 0
+word_list = []
+
+# First_page
 form_class_first = uic.loadUiType("GUI_files\First_page.ui")[0]
 class First_page(QMainWindow, form_class_first):
     def __init__(self):
         super().__init__()
-        self.dialogs = list()
         self.setUI()
 
     def setUI(self):
@@ -31,8 +37,8 @@ class First_page(QMainWindow, form_class_first):
         pass_the_stage = 1
         self.close()
 
+# Second_page
 form_class_second = uic.loadUiType("GUI_files\Second_page.ui")[0]
-selcected_level = 0
 class Second_page(QMainWindow, form_class_second):
     def __init__(self):
         super().__init__()
@@ -47,45 +53,42 @@ class Second_page(QMainWindow, form_class_second):
         self.m_3_button.clicked.connect(self.button_click_m_3)
 
     def button_click_e_5(self):
-        global selcected_level
+        global selected_level
         global pass_the_stage
-        selcected_level = 1
+        selected_level = 1
         pass_the_stage = 1
         self.close()
 
     def button_click_e_6(self):
-        global selcected_level
+        global selected_level
         global pass_the_stage
-        selcected_level = 2
+        selected_level = 2
         pass_the_stage = 1
         self.close()
 
     def button_click_m_1(self):
-        global selcected_level
+        global selected_level
         global pass_the_stage
-        selcected_level = 3
+        selected_level = 3
         pass_the_stage = 1
         self.close()
 
     def button_click_m_2(self):
-        global selcected_level
+        global selected_level
         global pass_the_stage
-        selcected_level = 4
+        selected_level = 4
         pass_the_stage = 1
         self.close()
 
     def button_click_m_3(self):
-        global selcected_level
+        global selected_level
         global pass_the_stage
-        selcected_level = 5
+        selected_level = 5
         pass_the_stage = 1
         self.close()
 
 # Third_page
 form_class_third = uic.loadUiType("GUI_files\Third_page.ui")[0]
-
-selected_word = "치킨"
-now_check_word = "\0"
 class Third_page(QMainWindow, form_class_third):
     def __init__(self):
         super().__init__()
@@ -99,7 +102,6 @@ class Third_page(QMainWindow, form_class_third):
         print(step)
         if step == 1:
             now_check_word = selected_word
-            gwd.Get_Need_Content(selected_word)
             self.text_to_user.setText(now_check_word)
             self.answer_true_false.setText("단어를 입력해 주세요.")
             self.step_show.setText("Step : 1")
@@ -123,8 +125,13 @@ class Third_page(QMainWindow, form_class_third):
         global now_check_word
         global step
         print(now_check_word)
-        if(self.enter_text.toPlainText() != now_check_word):
-            self.answer_true_false.setText("입력하신 단어에 오타가 존재합니다.")
+        if(self.enter_text.toPlainText() != now_check_word ):
+            if step == 1:
+                self.answer_true_false.setText("입력하신 단어에 오타가 존재합니다.")
+            if step == 2:
+                self.answer_true_false.setText("입력하신 문장에 오타가 존재합니다.")
+            if step == 3:
+                self.answer_true_false.setText("입력하신 문장에 오타가 존재합니다.")
         else:
             if step == 1:
                 step = 2
@@ -144,12 +151,20 @@ class Third_page(QMainWindow, form_class_third):
                 return
             if step == 3:
                 global pass_the_stage
+                global selected_word
+                global word_list
                 pass_the_stage = 1
+                if i != len(word_list) - 1:
+                    selected_word = word_list[i+1]
+                    gwd.Get_Need_Content(selected_word)
                 self.close()
+
 
 def Control_GUI():
     global pass_the_stage
     global step
+    global i
+    global selected_word
     global stage
     print("Starting the lexical_learning_program!")
     print("Do not exit this tab if you want to use the program!")
@@ -159,7 +174,10 @@ def Control_GUI():
     Start_Second_GUI()
     check_x_button(2)
     pass_the_stage = 0
-    while 1:
+    get_word_from_file()
+    selected_word = word_list[0]
+    gwd.Get_Need_Content(selected_word)
+    for i in range(len(word_list)):
         step = 1
         Start_Third_GUI()
         check_x_button(3)
@@ -196,7 +214,34 @@ def check_x_button(stage):
                 Start_Third_GUI()
                 check_x_button(3)
 
+def get_word_from_file():
+    global word_list
+    global selected_level
+    if selected_level == 1:
+        print("elementary 5 - not builted")
+        print("file_loading failed")
+        exit(0)
+    elif selected_level == 2:
+        print("elementary 6 - not builted")
+        print("file_loading failed")
+        exit(0)
+    elif selected_level == 3:
+        fp = open("Data\data_m1.dat","rt",encoding = "UTF-8")
+    elif selected_level == 4:
+        fp = open("Data\data_m2.dat","rt",encoding = "UTF-8")
+    else:
+        fp = open("Data\data_m3.dat","rt",encoding = "UTF-8")
+    word_list = fp.readlines()
+    for i in range(len(word_list)-1):
+        word_list[i] = word_list[i][:len(word_list[i])-1]
+    fp.close()
+    random.shuffle(word_list)
+
+
+
+
 
 if __name__ == "__main__":
+    #Start_Loading_GUI()
     Control_GUI() #This line used for test GUI_management.py
                   #Remove it when editing GUI_management.py is finished.
